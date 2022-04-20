e3c97987e11481edccfacc82a23ee293ed3ab289
Reinhard Gruber <r.gruber@smartinmedia.com>
2022-04-20 12:19:04 +0200

    some rendering benchmarks

--- Tests/FO-DICOM.Benchmark/Program.cs
+++ Tests/FO-DICOM.Benchmark/Program.cs
@@ -1,6 +1,7 @@
-﻿// Copyright (c) 2012-2021 fo-dicom contributors.
+﻿// Copyright (c) 2012-2022 fo-dicom contributors.
 // Licensed under the Microsoft Public License (MS-PL).
 
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 
 namespace FellowOakDicom.Benchmark
@@ -10,7 +11,13 @@ namespace FellowOakDicom.Benchmark
         static void Main()
         {
             // Run all benchmarks in assembly
-            BenchmarkRunner.Run(typeof(Program).Assembly);
+            // BenchmarkRunner.Run(typeof(Program).Assembly);
+
+            BenchmarkRunner.Run<RenderImageBenchmark>(
+                ManualConfig.Create(DefaultConfig.Instance)
+                .WithOptions(ConfigOptions.JoinSummary)
+                .WithOptions(ConfigOptions.DisableOptimizationsValidator)
+                );
         }
     }
 }
